50f608bddb95c66e54b31c7c84b84be49e3b5b7f
Erik Tholen <ltd180@gmail.com>
2016-10-27 23:28:42 +0200

    Add CTK comment in top of every file

--- catalog/browse/catalog-company.filter.js
+++ catalog/browse/catalog-company.filter.js
@@ -1,3 +1,7 @@
+// Developed by Chalmers Teknologkonsulter (CTK) for DatE-IT in 2016
+// Tel: 031-772 8332
+// Mail: info(at)ctk(dot)se
+
 angular.module('dateitse.catalog.company-filter', [])
 
   .filter('companyFilter', companyFilter)
